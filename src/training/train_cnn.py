2cdcef4cbe592b0b7ae3eb823000bde6134a5c32
Reid W <reiddwilliamson@gmail.com>
2025-07-15 17:19:40 -0700

    created basic training/validation loopwith k-fold cross-validation

---
+++ src/training/train_cnn.py
@@ -0,0 +1,87 @@
+from cnn_models import *
+import torch.optim as optim
+from sklearn.model_selection import KFold
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset, Subset
+import numpy as np
+import torch
+
+k: int = 5
+batch_size: int = 128
+epochs: int = 100
+filepath: str = '../data/data.npy'
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+train_dataset = np.load(file=filepath)
+kfold = KFold(n_splits=k)
+
+for fold, (train_idx, val_idx) in enumerate(kfold.split(train_dataset)):
+    print(f"Fold {fold+1}/{k}")
+    train_subset = Subset(train_dataset, train_idx)
+    val_subset = Subset(train_dataset, val_idx)
+    train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_subset, batch_size=batch_size)
+
+    # TODO: fill this out with hyperparameters
+    model: CNN = CNN()
+    model.to(device=device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters())
+
+    train_loss: float = 0.0
+    val_loss: float = 0.0
+    total_val_samples: int = 0
+    total_train_samples: int = 0
+
+    model.train()
+    for epoch in range(epochs):
+        print(f"Epoch [{epoch+1}/{epochs}]")
+        running_loss: float = 0.0
+
+        for i, data in enumerate(train_loader):
+
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            batch_size: int = inputs.size(0)
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item() * batch_size
+            total_train_samples += batch_size
+
+        train_loss += running_loss
+    
+    train_loss /= total_train_samples
+
+    model.eval()
+    with torch.no_grad():
+        for _, data in enumerate(val_loader):
+
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            batch_size: int = inputs.size(0)
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            val_loss += loss.item() * batch_size
+            total_val_samples += batch_size
+    
+    val_loss /= total_val_samples
+
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'training_loss': train_loss,
+        'validation_loss': val_loss
+    }, f"models/model_fold_{fold+1}.pth")
